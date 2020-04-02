6e4093cc1b6dd2fb889a0cc7b5d939a766201ac9
George <gdionascs@gmail.com>
2020-04-01 22:42:58 -0400

    Added dataframe structure

--- financialScrape.py
+++ financialScrape.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import numpy as np
+import pandas as pd
 
 symb = input("Please enter stock symbol >> ")
 
@@ -11,17 +13,19 @@ def finScrape(symbol):
     values = soup.findAll('td',{"class": 'valueCell'})
     titles = soup.findAll('td',{'class': 'rowTitle'})
 
-    scrapeData = ["Measure", "2015", "2016", "2017", "2018", "2019"]
-
+    scrapeData = np.array([["Measure", "2015", "2016", "2017", "2018", "2019"]])
+    
     q = 0
     for i in titles:
-        scrapeData.append(str(re.sub(re.compile('<.*?>'), '', str(i))).strip())
+        data = []
+        data.append(str(re.sub(re.compile('<.*?>'), '', str(i))).strip())
         for x in range(q, q+5):
-            scrapeData.append(str(re.sub(re.compile('<.*?>'), '', str(values[x]))).strip())
+            data.append(str(re.sub(re.compile('<.*?>'), '', str(values[x]))).strip())
+        data = [data]
+        scrapeData = np.append(scrapeData,data,axis=0)
         q = q + 5
+    scrapeData = pd.DataFrame(data=scrapeData[1:,1:], index=scrapeData[1:,0], columns=scrapeData[0,1:])
 
-    for i in range(len(scrapeData)):
-        if (i % 6 == 0):
-            print(scrapeData[i]+" "+scrapeData[i+1]+" "+scrapeData[i+2]+" "+scrapeData[i+3]+" "+scrapeData[i+4]+" "+scrapeData[i+5])
+    print(scrapeData)
 
 finScrape(symb)
